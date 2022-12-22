b1bb6d670763ce33e6e187de7f542c3c5faa940b
saimanmoreno <simaojmoreno@gmail.com>
2022-12-22 17:34:11 +0000

    readme added

---
+++ README.md
@@ -0,0 +1,7 @@
+# Aplicação Simples de Tarefas @nhasTarefas
+
+Uma aplicação simples de tarefas, feito usando PHP e MySQL no servidor, AJAX para envios ao servidor de forma assincrona, e HTML, BootStrap e Jquery. O intuito é relembrar conceitos básicos de web sem uso de frameworks.
+
+# Screenshoot
+
+![alt text](https://github.com/saimanmoreno/mercado-cryptomoedas/blob/main/assets/imgs/screenshoot.png)
--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,98 +1,10 @@
 $(document).ready(function () {
 
     $('#search-result').hide();
-
     listTasks();
 
-    // Todo: esconder o card de resultados da pesquisa quando não for encontrado valores correspondentes ou mostrar mensagem de sem sucesso
-    $('#search').keyup(searchTasks);
-    $('#search').on('click', () => {
 
-        $('#search').val('');
-
-        searchTasks();
-
-    });
-
-    function searchTasks(e) {
-
-        let searchText = $('#search').val();
-
-        console.log(searchText);
-
-        if (searchText) {
-            $.ajax({
-                url: 'server/controller.php',
-                type: 'POST',
-                data: { action: 'searchTasks', searchText },
-                success: function (response) {
-
-                    let tasksSearch = JSON.parse(response);
-                    template = '';
-
-                    if (tasksSearch.length > 0) {
-                        tasksSearch.forEach(task => {
-                            template += `
-                            <li>
-                                ${task.name}
-                            </li>
-                        `});
-                    } else {
-                        template += `
-                            <div class="row">
-                                <p class="my-3 d-flex justify-content-center">Sem resultados!</p>
-                            </div>
-                        `;
-                    }
-
-                    $('#container').html(template);
-                    $('#search-result').show();
-
-                }
-            })
-        } else {
-            $('#search-result').hide();
-        }
-    }
-
-
-
-    $('#task-form').submit(function (e) {
-
-        e.preventDefault();
-
-        const name = $('#name').val();
-
-        // caso desativem o required
-        if (!name) return;
-
-        const postData = {
-            name: $('#name').val(),
-            description: $('#description').val(),
-            action: 'createTask'
-        }
-
-        var submitButton = $('#submit-button');
-
-        submitButton.text('Criando...');
-        submitButton.prop("disabled", true);
-
-        $.post('server/controller.php', postData, response => {
-            console.log(response);
-
-            $('#task-form').trigger('reset');
-        })
-
-        // 1 second delay
-        setTimeout(function () {
-            submitButton.text('Criar Tarefa');
-            submitButton.prop("disabled", false);
-            listTasks();
-        }, 500);
-    })
-
-
-
+    // listar as tarefas
     function listTasks() {
 
         $.ajax({
@@ -135,4 +47,91 @@ $(document).ready(function () {
             }
         })
     }
+
+
+    // pesquisar as tarefas
+    $('#search').keyup(searchTasks);
+    $('#search').on('click', () => {
+
+        $('#search').val('');
+
+        searchTasks();
+
+    });
+    function searchTasks(e) {
+
+        let searchText = $('#search').val();
+
+        console.log(searchText);
+
+        if (searchText) {
+            $.ajax({
+                url: 'server/controller.php',
+                type: 'POST',
+                data: { action: 'searchTasks', searchText },
+                success: function (response) {
+
+                    let tasksSearch = JSON.parse(response);
+                    template = '';
+
+                    if (tasksSearch.length > 0) {
+                        tasksSearch.forEach(task => {
+                            template += `
+                            <li>
+                                ${task.name}
+                            </li>
+                        `});
+                    } else {
+                        template += `
+                            <div class="row">
+                                <p class="my-3 d-flex justify-content-center">Sem resultados!</p>
+                            </div>
+                        `;
+                    }
+
+                    $('#container').html(template);
+                    $('#search-result').show();
+
+                }
+            })
+        } else {
+            $('#search-result').hide();
+        }
+    }
+
+
+    // adicionar nova tarefa
+    $('#task-form').submit(function (e) {
+
+        e.preventDefault();
+
+        const name = $('#name').val();
+
+        // caso desativem o required
+        if (!name) return;
+
+        const postData = {
+            name: $('#name').val(),
+            description: $('#description').val(),
+            action: 'createTask'
+        }
+
+        var submitButton = $('#submit-button');
+
+        submitButton.text('Criando...');
+        submitButton.prop("disabled", true);
+
+        $.post('server/controller.php', postData, response => {
+            console.log(response);
+
+            $('#task-form').trigger('reset');
+        })
+
+        // 1 second delay
+        setTimeout(function () {
+            submitButton.text('Criar Tarefa');
+            submitButton.prop("disabled", false);
+            listTasks();
+        }, 500);
+    })
 })
